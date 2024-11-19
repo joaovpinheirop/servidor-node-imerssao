c9d3143574b7298c644b18b8ec8db098c793967c
joaovpinheiro <joaovpinheiros@gmail.com>
2024-11-19 20:30:51 -0300

    Criando um endPoint para retornar apenas um elemento da lista

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 import express from 'express'; // importar express
-const app = express();
 
+//Lista de Posts
 const posts = [
   {
+      id: 1,    
       descricao: 'Gato brincando com um novelo de lã',
       imagem: 'https://placekitten.com/400/200'
   },
   {
+      id: 2,
       descricao: 'Paisagem de um pôr do sol',
       imagem: 'https://source.unsplash.com/random/400x300/?sunset'
   },
   {
+      id: 3,
       descricao: 'Cachorro correndo na praia',
       imagem: 'https://source.unsplash.com/random/400x300/?dog,beach'
   },
   {
+       id: 4,
       descricao: 'Comida deliciosa',
       imagem: 'https://source.unsplash.com/random/400x300/?food'
   },
-  {
-      // Adicione mais objetos aqui, com suas próprias descrições e imagens
-      descricao: 'Seu post aqui',
-      imagem: 'Seu link para a imagem aqui'
-  }
 ];
+
+const app = express();
+app.use(express.json());
+
 // Servidor local sendo executado na porta 3000.
 app.listen(3000,()=>{
   console.log('servidor escutando...');
 });
 
-// Rota (/api) | Get Obter uma informação
-app.get('/api',(req, res)=>{
-  res.status(200).send("Rota Inicial");
+// ENDPOINT | ROTA (/posts) | Obter lista
+app.get('/posts',(req, res)=>{
+  res.status(200).json(posts);// converter para json
 });
+
+// Buscar Posts Por Id
+function BuscarPostPorId(id){
+  return posts.findIndex((post)=>{
+    return post.id === Number(id);
+  });
+}
+
+// ENDPOINT | ROTA (/posts) | Obter um objeto por id
+app.get('/posts/:id',(req, res)=>{
+  const index = BuscarPostPorId(req.params.id)
+  res.status(200).json(posts[index]);// converter para json
+});
+
